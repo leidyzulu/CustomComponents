6fb3b36d019b9a0ef52fd94d9613e9254d8c1dc5
Leidy <leidy@Leidys-iMac.local>
2019-02-22 10:58:33 -0500

    refactor phone

--- app/src/main/java/com/example/myapplication/textField/EditTextFormField.kt
+++ app/src/main/java/com/example/myapplication/textField/EditTextFormField.kt
@@ -2,51 +2,25 @@ package com.example.myapplication.textField
 
 import android.content.Context
 import android.support.design.widget.TextInputLayout
+import android.text.TextWatcher
 import android.util.AttributeSet
 import android.widget.EditText
-import com.evercheck.wallet.utils.masks.DateTextWatcherMask
-import com.example.myapplication.*
 import com.example.myapplication.formfield.ValidationResult
 import com.example.myapplication.helper.*
-import com.example.myapplication.helper.masks.PhoneNumberTextWatcherMask
+import java.util.regex.Pattern
 
 
-class EditTextFormField constructor(context: Context, attrs: AttributeSet) :
+abstract class EditTextFormField constructor(context: Context, attrs: AttributeSet) :
     TextInputLayout(context, attrs), ITextFormField {
 
 
-    var mTypeField: Int? = null
-    var mEditText: EditText? = null
-
-    init {
-        context.theme.obtainStyledAttributes(
-            attrs,
-            R.styleable.EditTextFormField,
-            0, 0
-        ).apply {
-            try {
-                mTypeField = getInt(R.styleable.EditTextFormField_regex, -1)
-            } finally {
-                recycle()
-            }
-        }
-
-    }
+    protected var mRegex: String? = null
+    private var mEditText: EditText? = null
 
     override fun onFinishInflate() {
         super.onFinishInflate()
 
         mEditText = this.editText
-
-
-        when (mTypeField) {
-            OPTION_PHONE -> mEditText?.apply {
-                addTextChangedListener(PhoneNumberTextWatcherMask(this))
-            }
-            OPTION_DATE -> mEditText?.apply {
-                addTextChangedListener(DateTextWatcherMask(this))
-            }
-        }
 
     }
 
@@ -57,10 +31,8 @@ class EditTextFormField constructor(context: Context, attrs: AttributeSet) :
                 false,
                 VALIDATE_EMPTY
             )
-            !ValidatorHelper.isValidPhone(mEditText?.text) -> ValidationResult(
-                false,
-                VALIDATE_NUMERIC
-            )
+            !Pattern.compile(mRegex).matcher(mEditText?.text.toString()).matches() -> getErrorMessage()
+
             else -> ValidationResult(true, EMPTY)
         }
     }
@@ -74,5 +46,7 @@ class EditTextFormField constructor(context: Context, attrs: AttributeSet) :
         this.isErrorEnabled = false
         this.error = EMPTY
     }
+
+    abstract fun getErrorMessage(): ValidationResult
 
 }
---
+++ app/src/main/java/com/example/myapplication/textField/EditTextPhoneField.kt
@@ -0,0 +1,43 @@
+package com.example.myapplication.textField
+
+import android.content.Context
+import android.util.AttributeSet
+import com.example.myapplication.R
+import com.example.myapplication.formfield.ValidationResult
+import com.example.myapplication.helper.masks.PhoneNumberTextWatcherMask
+
+class EditTextPhoneField(context: Context, attrs: AttributeSet) : EditTextFormField(context, attrs) {
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.EditTextFormField,
+            0, 0
+        ).apply {
+            try {
+                mRegex = getString(R.styleable.EditTextFormField_regex)
+            } finally {
+                recycle()
+            }
+        }
+
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+         this.editText?.apply {
+             addTextChangedListener(PhoneNumberTextWatcherMask(this))
+         }
+
+    }
+
+
+    override fun getErrorMessage(): ValidationResult {
+       return ValidationResult(false, "phone")
+    }
+
+
+
+
+}
