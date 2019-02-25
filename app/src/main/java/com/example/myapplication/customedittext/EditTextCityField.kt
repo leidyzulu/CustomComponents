f6b6cbf562530cf2bac8be22ec84d3013b3196f8
oscar gallon <ogallon@condorlabs.io>
2019-02-25 17:59:01 -0500

    fix

--- app/src/main/java/com/example/myapplication/customedittext/EditTextCityField.kt
+++ app/src/main/java/com/example/myapplication/customedittext/EditTextCityField.kt
@@ -5,24 +5,42 @@ import android.util.AttributeSet
 import com.example.myapplication.formfield.ValidationResult
 import com.example.myapplication.helper.EMPTY
 import com.example.myapplication.helper.VALIDATE_CITY_ERROR
+import com.example.myapplication.helper.ZERO
 
 /**
  * @author Oscar Gallon on 2/25/19.
  */
 class EditTextCityField(context: Context, attrs: AttributeSet) : EditTextFormField(context, attrs) {
 
-    private val mStateName: String? = null
-    private val mCities: List<String>? = null
+    private var mStateName: String? = null
+    private var mCities: List<String>? = null
 
     override fun getMessage(): ValidationResult {
         return ValidationResult(false, "$VALIDATE_CITY_ERROR ${mStateName ?: EMPTY}")
     }
 
     override fun isValid(): ValidationResult {
-        if(mCities?.contains(editText?.text ?: EMPTY) == false) {
-          return ValidationResult(false, VALIDATE_CITY_ERROR)
+
+        val emptyValidation = super.isValid()
+
+        if(!emptyValidation.isValid){
+            return emptyValidation
         }
 
-        return super.isValid()
+        if(mCities?.filter {
+            it.toLowerCase() == editText?.text?.toString()?.toLowerCase() ?: EMPTY
+        }?.count() ?: ZERO <= ZERO){
+            return ValidationResult(false, VALIDATE_CITY_ERROR)
+        }
+
+        return emptyValidation
+    }
+
+    fun setCities(cities: List<String>){
+        mCities = cities
+    }
+
+    fun setStateName(state: String){
+        mStateName = state
     }
 }
